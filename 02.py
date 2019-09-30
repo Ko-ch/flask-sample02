9c8445796a5c74c854cc44d21fa43c75177b0e71
Koichiro <koichiro_takagi@outlook.jp>
2019-09-30 22:30:01 +0900

    ずっと見てられないおみくじサイトを作成しました。 これは楽しい。 スピードはどこまでもあがるな。

--- 02.py
+++ 02.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,6 +13,14 @@ def index():
 @app.route("/user/<name>")
 def hello(name):
     return render_template("hello.html", name=name)
+
+
+@app.route("/omikuji")
+def omikuji():
+    kuji = ["大吉", "中吉", "吉", "小吉", "末吉", "凶"]
+    result = random.choice(kuji)
+    return render_template("omikuji.html", result=result)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
